6802d0d2bed8d65b00483d47ec723a55457778ff
tohshima115 <tohshima115@gmail.com>
2024-01-18 04:21:04 +0900

    Updated styles for Name, Planet, and Stars components

--- src/components/Planet.jsx
+++ src/components/Planet.jsx
@@ -1,13 +1,25 @@
 import './Planet.css';
+import { useState, useEffect } from 'react'; 
 
 function Planet() {
   const isPathnameNotEmpty = window.location.pathname !== "/";
+  const [randomLetter, setRandomLetter] = useState('a'); // 初期値を'a'としてランダムな文字を生成するstateを作成
+
+  useEffect(() => {
+    const alphabet = 'abcdefghi';
+    const randomIndex = Math.floor(Math.random() * alphabet.length); // 0から9までのランダムなインデックスを生成
+    const randomChar = alphabet[randomIndex]; // ランダムな文字を取得
+    setRandomLetter(randomChar);
+  }, []); // 空の依存配列を渡すこで初回レンダリング時にのみ実行
+
+  console.log(randomLetter)
+
   return (
     <div className={`container ${isPathnameNotEmpty ? "blur" : ""}`}>
       <div className='sun'>
         <div className='earth'>
           <div className='earth-locus'></div>
-          <div className='earth-main'>
+          <div className={`earth-main ${randomLetter}`}>
             <div className='moon'>
               <div className='moon-locus outside'></div>
               <div className='moon-locus'></div>
